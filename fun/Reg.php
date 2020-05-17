f183dd70d814ee9690c6a793f3e04d6b99da6c48
x-odi <62629048+x-odi@users.noreply.github.com>
2020-05-17 23:49:08 +0300

    Update Reg.php

--- fun/Reg.php
+++ fun/Reg.php
@@ -8,20 +8,18 @@ $result=$conn->query($sql);
 while($row=$result->fetch_assoc()){ 
   $count= $result->num_rows;   
 }  
-if($count >0){ 
-header('Location:http://192.168.1.50:8280/gems11/Reg.php?q1=1');
+if($count >0){ header('Location:http://192.168.1.50:8280/gems11/Reg.php?q1=1');
 exit();
 }
-
-$Email=$_GET['Email'];
-$CompanyName=$_GET['CompanyName'];
-$Mobile=$_GET['Mobile'];
 $pass1=$_GET['pass1'];
 $pass2=$_GET['pass2'];  
-if($pass1 !== pass2){ 
-    header('Location:http://192.168.1.50:8280/gems11/Reg.php?q1=2');
+if($pass1 !== $pass2){ header('Location:http://192.168.1.50:8280/gems11/Reg.php?q1=2');
     exit();
    }
+
+   $Email=$_GET['Email'];
+$CompanyName=$_GET['CompanyName'];
+$Mobile=$_GET['Mobile'];
 
 $sql = "INSERT INTO reg (UserName, Email, CompanyName, Mobile, pass1)
 VALUES ('$UserName', '$Email', '$CompanyName','$Mobile','$pass1')";
